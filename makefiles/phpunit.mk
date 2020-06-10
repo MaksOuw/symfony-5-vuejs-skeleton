3b520e53a3b9790778ae32dc2cdfc99c2c43a4b7
Maxime Charron <mcharron@naoned.fr>
2020-06-10 21:30:44 +0200

    fix: unit test now launch

--- makefiles/phpunit.mk
+++ makefiles/phpunit.mk
@@ -1,10 +1,5 @@
-CONTAINER_SOURCE_PATH=/var/www/app
-
-phpunit = $(DOCKER_RUN) --rm --name phpunit \
-			-v ${HOST_SOURCE_PATH}:${CONTAINER_SOURCE_PATH} \
-			-w ${CONTAINER_SOURCE_PATH} \
-			-u ${USER_ID}:${GROUP_ID} \
-			app/bin/phpunit ${1}
+phpunit = docker-compose -f docker/docker-compose.yml run \
+			front php vendor/bin/phpunit
 
 phpunit:
 	@$(call phpunit, )
